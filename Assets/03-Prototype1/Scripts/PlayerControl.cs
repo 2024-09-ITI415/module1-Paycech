0b93090e65a30fa3e968b46a78dabf13a3e5757b
Paycech <paycech@gmail.com>
2024-10-16 03:04:27 -0400

    prototype start

--- Assets/03-Prototype1/Scripts/PlayerControl.cs
+++ Assets/03-Prototype1/Scripts/PlayerControl.cs
@@ -1,18 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerControl : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Rigidbody of the player.
+    private Rigidbody rb;
+
+    // Movement along X and Y axes.
+    private float movementX;
+    private float movementY;
+
+    // Speed at which the player moves.
+    public float speed = 0;
+    public float jump = 0;
+    public bool onGround = true;
+
+    // Start is called before the first frame update.
     void Start()
     {
-        
+        // Get and store the Rigidbody component attached to the player.
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    // This function is called when a move input is detected.
+    void OnMove(InputValue movementValue)
     {
-        
+        // Convert the input value into a Vector2 for movement.
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        // Store the X and Y components of the movement.
+        movementX = movementVector.x;
+        movementY = movementVector.y;
     }
+
+    void Update ()
+     {
+        if (Input.GetButtonDown("Jump") && onGround)
+        {
+            rb.AddForce(Vector3.up * jump, ForceMode.Impulse);
+            onGround = false;
+        }
+     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            onGround = true;
+        }
+    }
+    
+
+    // FixedUpdate is called once per fixed frame-rate frame.
+    private void FixedUpdate()
+    {
+        // Create a 3D movement vector using the X and Y inputs.
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        // Apply force to the Rigidbody to move the player.
+        rb.AddForce(movement * speed);
+    }
+
+    
 }
